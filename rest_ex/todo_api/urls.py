e8b5ee39843df735109a09f777c9f7a6a808d118
LukeWait <123434265+LukeWait@users.noreply.github.com>
2024-08-02 18:47:38 +1000

    removed rest_ex

--- rest_ex/todo_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TodoListApiView, TodoDetailApiView
-
-urlpatterns = [
-    path('', TodoListApiView.as_view()),  # Default path for listing and creating todos
-    path('<int:todo_id>/', TodoDetailApiView.as_view()),  # Path for retrieving, updating, and deleting a specific todo
-]
